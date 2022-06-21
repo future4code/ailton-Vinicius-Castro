7c7f2d55e4ad70742b6756bbf335bf891c422364
Vincius CAstro <38039737+Castrovinnie@users.noreply.github.com>
2022-06-21 17:55:53 -0300

    encaminhando apis-labenusers

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <p>
+          <button>Trocar de tela</button>
+          <input></input>
+          <input></input>
+          <button>Criar usuario</button>
+       </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
