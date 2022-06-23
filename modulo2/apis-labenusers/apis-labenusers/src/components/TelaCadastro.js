1e0937be3d589688899c10a746acbfbd33864cd2
Vincius CAstro <38039737+Castrovinnie@users.noreply.github.com>
2022-06-23 09:55:13 -0300

    encaminhando apis-labenusers atualizando, resolvi o erro substituindo {} por ()

--- modulo2/apis-labenusers/apis-labenusers/src/components/TelaCadastro.js
+++ modulo2/apis-labenusers/apis-labenusers/src/components/TelaCadastro.js
@@ -1,47 +1,66 @@
-
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 export default class TelaCadastro extends React.Component {
+  state = {
+    nome: "",
+    email: "",
+  };
 
-    state = {
-        nome: "",
-        email:""
-    }
+  handleNome = (event) => {
+    this.setState({ nome: event.target.value });
+  };
 
-  // handleNome = {e} => {
-  //   this.setState({nome:e.target.value})
-  // }
+  handleEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
 
-  // handleEmail = {e} => {
-  //   this.setState({email:e.target.value})
-  // }
-  
-  // fazerCadastro ()  => {
-  //   console.log(this.state)
+  fazerCadastro = () => {
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+    const body = {
+      name: this.state.nome,
+      email: this.state.email,
+    };
 
-  // }
+    axios
+      .post(url, body, {
+        headers: {
+          Authorization: "vinicius-castro-ailton",
+        },
+      })
 
+      .then((res) => {
+        alert("Usuário cadastrado com sucesso!");
+        this.setState({ nome: "", email: "" });
+      })
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.irParaLista}>Ir para Lista de Usuarios</button>
-                    <h2>Cadastro</h2>
-                <input placeholder={'Nome'}
-                     value={this.state.name}
-                     onChange={this.handleNome}
-                />
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
 
-                <input 
-                    placeholder={'E-Mail'}
-                    value={this.state.email}
-                    onChange={this.handle}
-                />          
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.irParaLista}>
+          Ir para Lista de Usuarios
+        </button>
+        <h2>Cadastro</h2>
+        <input
+          placeholder={"Nome"}
+          value={this.state.name}
+          onChange={this.handleNome}
+        />
 
-            <button onClick={this.fazerCadastro}>Cadastrar</button>
-        
-        </div>
-    )
-        }
-      }
-      
+        <input
+          placeholder={"E-Mail"}
+          value={this.state.email}
+          onChange={this.handleEmail}
+        />
+
+        <button onClick={this.fazerCadastro}>Cadastrar</button>
+      </div>
+    );
+  }
+}
