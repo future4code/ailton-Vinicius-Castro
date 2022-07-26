e4fb805eb2c4219702a772790cbe8699378af023
Vincius CAstro <38039737+Castrovinnie@users.noreply.github.com>
2022-07-26 16:57:07 -0300

    encaminhando arqivos da pagina de login, ficou estilizado e pronto para receber a funcao

--- modulo3/05 - projeto-labEddit/labeddit-app/src/App.js
+++ modulo3/05 - projeto-labEddit/labeddit-app/src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
-
-class App extends Component {
-  render() {
-    return <div>
-      Olá mundo
-      </div>;
-  }
+import React from "react";
+// import FeedPage from "./pages/FeedPage"
+// import LoginPage from "./pages/LoginPage"
+// import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+// import PostPage from "./pages/PostPage/PostPage";
+import Routes from "./routes/Routes";
+const App = () => {
+  return (
+    <div>
+     <Routes/>
+    </div> 
+  )
 }
 
 export default App;
