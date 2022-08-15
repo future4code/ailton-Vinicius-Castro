b56e0edb8560f92ce6f0645237f9f625bc10881d
Vincius CAstro <38039737+Castrovinnie@users.noreply.github.com>
2022-08-15 19:00:10 -0300

    Encaminhando arquivos intro express

---
+++ modulo4/05 - intro-express/src/index.ts
@@ -0,0 +1,3 @@
+// - Exercício 1
+    
+//     Faça a instalação e configuração do Express na pasta do exercício. Para testar, crie um endpoint que aponte para a URL base da API. Esse endpoint pode responder apenas com um status ou mensagem fixa.
